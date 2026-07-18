124034d4ffb54dec051e552b462095a4bb24eb45
Vishesh <visheshsinghbhadauriya547@gmail.com>
2026-07-18 06:47:06 +0530

    Add files via upload

---
+++ Automation-Testing/13th-July/Constructors in java/src/constructors/C_01_Constructor.java
@@ -0,0 +1,22 @@
+package constructors;
+
+public class C_01_Constructor {
+	//Constructor
+	public C_01_Constructor() {
+		System.out.println("This is the Constructor block");
+		
+	}
+	//Method
+	public void method() {
+		System.out.println("This is the Method block");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C_01_Constructor obj =new C_01_Constructor();
+		obj.method();
+		
+
+	}
+
+}
