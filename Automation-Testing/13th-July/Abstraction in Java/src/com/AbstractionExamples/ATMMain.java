e14c00f1309e91d682e28a6649243b489bb4a98b
Vishesh <visheshsinghbhadauriya547@gmail.com>
2026-07-18 06:44:43 +0530

    Add files via upload

---
+++ Automation-Testing/13th-July/Abstraction in Java/src/com/AbstractionExamples/ATMMain.java
@@ -0,0 +1,15 @@
+package com.AbstractionExamples;
+
+	public class ATMMain {
+
+	    public static void main(String[] args) {
+
+	        SBIATM atm = new SBIATM();
+
+	        atm.withdrawMoney();
+	        atm.depositMoney();
+	        atm.checkBalance();
+	    }
+	}
+
+
---
+++ Automation-Testing/13th-July/Abstraction in Java/src/com/AbstractionExamples/SBIATM.java
@@ -0,0 +1,16 @@
+package com.AbstractionExamples;
+
+	public class SBIATM extends ATM {
+
+	    @Override
+	    void withdrawMoney() {
+	        System.out.println("Money Withdrawn Successfully.");
+	    }
+
+	    @Override
+	    void depositMoney() {
+	        System.out.println("Money Deposited Successfully.");
+	    }
+	}
+
+
